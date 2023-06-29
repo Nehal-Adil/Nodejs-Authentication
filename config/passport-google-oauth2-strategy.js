f0f5a97270be8de3e2b2dafa9194dd4455e9ad14
Nehal <mdnehaladil@gmail.com>
2023-06-29 23:19:32 +0530

    async await issues

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -15,39 +15,21 @@ passport.use(
     },
     function (accessToken, refreshToken, profile, done) {
       // Find a user
-      User.findOne({ email: profile.emails[0].value }).exec(function (
-        err,
-        user
-      ) {
-        if (err) {
-          console.log("Error in Google strategy-passport", err);
-          return;
-        }
-
+      User.findOne({ email: profile.emails[0].value }).then(function (user) {
         if (user) {
           // If found, set this user as req.user and send confirmation email
           googleMailer.google(user);
           return done(null, user);
         } else {
           // If not found, create the user and set it as req.user, then send confirmation email
-          User.create(
-            {
-              name: profile.displayName,
-              email: profile.emails[0].value,
-              password: crypto.randomBytes(20).toString("hex"),
-            },
-            function (err, user) {
-              if (err) {
-                console.log(
-                  "Error in creating user Google strategy-passport",
-                  err
-                );
-                return;
-              }
-              googleMailer.google(user);
-              return done(null, user);
-            }
-          );
+          User.create({
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            password: crypto.randomBytes(20).toString("hex"),
+          }).then((user) => {
+            googleMailer.google(user);
+            return done(null, user);
+          });
         }
       });
     }
--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -44,11 +44,7 @@ passport.serializeUser(function (user, done) {
 
 // Deserializing the user from the key in the cookies
 passport.deserializeUser(function (id, done) {
-  User.findById(id, function (err, user) {
-    if (err) {
-      console.log("Error in finding user ---> Passport");
-      return done(err);
-    }
+  User.findById(id).then((user) => {
     return done(null, user);
   });
 });
